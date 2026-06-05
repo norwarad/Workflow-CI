531fa62c335422959c0acf8c40f47239ad5c7141
Dede Norwara <d.norwara@gmail.com>
2026-06-05 15:53:24 +0700

    Update Workflow CI dataset to gaming addiction

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -24,11 +24,11 @@ if __name__ == "__main__":
     data = pd.read_csv(file_path)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        data.drop("target", axis=1),
-        data["target"],
+        data.drop("addiction_binary", axis=1),
+        data["addiction_binary"],
         random_state=42,
         test_size=0.2,
-        stratify=data["target"]
+        stratify=data["addiction_binary"]
     )
 
     input_example = X_train.iloc[:5]
